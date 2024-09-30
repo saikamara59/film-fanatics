6591cf1ab208eba09868507fcb1d72ffea1307e8
Tamerlan Mustafayev <tamerlan.m.2000@gmail.com>
2024-09-30 18:32:45 -0400

    Make a new connection string for MONGODB

--- server.js
+++ server.js
@@ -1,50 +1,50 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const dotenv = require("dotenv")
+const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
 const path = require('path')
 
 
-const app = express();
 
 
 // App configs
 dotenv.config();
+const app = express();
 app.set("view engine", "ejs");
 
 
 const PORT = process.env.PORT || 4000 
 
-
+// middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method")); 
+app.use(morgan("dev")); 
+app.use(express.static(path.join(__dirname, "public")));
+  
 
 // mongoose connection 
 
-// mongoose.connect(process.env.MONGODB_URI); 
+mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on("connected", () => {
-  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+  console.log(`Connected to MongoDB.`);
 });
 
 mongoose.connection.on("error", (err) => {
-  console.log(err);
+  console.log(err); 
 });
-
-// middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride("_method"));
-app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "public")));
-
-
-
-
-app.listen(PORT,() => {
-  console.log(`Listening on port ${PORT}`);
-})
 
 app.get('/', (req, res) => {
   res.render('index')
+}) 
+ 
+
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 })
+
 
 
