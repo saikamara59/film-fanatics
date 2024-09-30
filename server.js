cb7b92ab722db4ffa28f71e2270bb4bad75c6761
Tamerlan Mustafayev <tamerlan.m.2000@gmail.com>
2024-09-29 20:38:25 -0400

    Add middleware, render test page for home route

--- server.js
+++ server.js
@@ -1,35 +1,49 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv")
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+const path = require('path')
 
-const methodOverride = require("method-override")
 
-// App = configs
 const app = express();
 
+
+// App configs
 dotenv.config();
+app.set("view engine", "ejs");
 
-const PORT = process.envPORT || 4000 
+
+const PORT = process.env.PORT || 4000 
 
 
-// mongoose connection event listener
+
+// mongoose connection 
+
+// mongoose.connect(process.env.MONGODB_URI); 
+
 mongoose.connection.on("connected", () => {
-    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
-  });
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
 
-  mongoose.connection.on("error", (err) => {
-    console.log(err);
-  });
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+});
 
-  
+// middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
 
-  
-  app.use(express.static(path.join(__dirname,"public")));
 
 
 
-  app.listen(PORT,() => {
-    console.log(`Listening on port ${PORT}`);
-  })
+app.listen(PORT,() => {
+  console.log(`Listening on port ${PORT}`);
+})
 
+app.get('/', (req, res) => {
+  res.send('Hello')
+})
 
