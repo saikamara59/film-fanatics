827c92561cedccd9ad3716a103b8bc5fe209333a
Tamerlan Mustafayev <tamerlan.m.2000@gmail.com>
2024-09-30 19:06:49 -0400

    make show and new files

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const path = require('path')
 dotenv.config();
 const app = express();
 app.set("view engine", "ejs");
-
+ 
 
 const PORT = process.env.PORT || 4000 
 
@@ -37,7 +37,7 @@ mongoose.connection.on("error", (err) => {
 
 app.get('/', (req, res) => {
   res.render('index')
-}) 
+})  
  
 
 
