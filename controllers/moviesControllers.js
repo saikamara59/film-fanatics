0afe9b3d9c25b1e2f7e6d1693636d7395aaa19be
saikamara59 <mhsaidu@gmail.com>
2024-10-01 19:09:40 -0400

    updated controllers function in server

--- controllers/moviesControllers.js
+++ controllers/moviesControllers.js
@@ -4,7 +4,7 @@ const movie = require("../models/movies");
 const getAllMovies = async (req,res) => {
     try {
         const allMovies = await movie.find();
-        res.render("movies/index" {movies: allMovies, message: "Hello World"});
+        res.render("movies/index", {movies: allMovies, message: "Hello World"});
     } catch (err) {
         console.log(err);
         res.redirect("/");
--- server.js
+++ server.js
@@ -4,12 +4,8 @@ const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
 const path = require('path')
-<<<<<<< HEAD
-const movieController = require("./controllers/moviesControllers");
+const movieController = require("./controllers/moviesControllers")
 
-
-=======
->>>>>>> 93ea442e6d13f7ed2c65757f30982dcaaf1ea9aa
 
 // App configs
 dotenv.config();
