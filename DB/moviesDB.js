65c056a05d4fbed510bc69e5af98bd0f34773aa1
Tamerlan Mustafayev <tamerlan.m.2000@gmail.com>
2024-10-02 20:59:10 -0400

    Make DB folder and Movies DB array

---
+++ DB/moviesDB.js
@@ -0,0 +1,55 @@
+const movies = [
+    {
+      id: 1,
+      title: "Inception",
+      year: 2010,
+      genre: "Sci-Fi",
+      rating: 8.8,
+      director: "Christopher Nolan",
+      stillInTheaters: false,
+      coverUrl: "https://m.media-amazon.com/images/I/81p+xe8cbnL._AC_SY679_.jpg"
+    },
+    {
+      id: 2,
+      title: "The Godfather",
+      year: 1972,
+      genre: "Crime, Drama",
+      rating: 9.2,
+      director: "Francis Ford Coppola",
+      stillInTheaters: false,
+      coverUrl: "https://upload.wikimedia.org/wikipedia/en/1/1c/Godfather_ver1.jpg"
+    },
+    {
+      id: 3,
+      title: "Parasite",
+      year: 2019,
+      genre: "Thriller, Drama",
+      rating: 8.5,
+      director: "Bong Joon Ho",
+      stillInTheaters: false,
+      coverUrl: "https://upload.wikimedia.org/wikipedia/en/5/53/Parasite_%282019_film%29.png"
+    },
+    {
+      id: 4,
+      title: "The Dark Knight",
+      year: 2008,
+      genre: "Action, Crime",
+      rating: 9.0,
+      director: "Christopher Nolan",
+      stillInTheaters: false,
+      coverUrl: "https://upload.wikimedia.org/wikipedia/en/8/8a/Dark_Knight.jpg"
+    },
+    {
+      id: 5,
+      title: "Pulp Fiction",
+      year: 1994,
+      genre: "Crime, Drama",
+      rating: 8.9,
+      director: "Quentin Tarantino",
+      stillInTheaters: false,
+      coverUrl: "https://upload.wikimedia.org/wikipedia/en/8/82/Pulp_Fiction_cover.jpg"
+    }
+  ];
+  
+  module.exports = movies;
+  
